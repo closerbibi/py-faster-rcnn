7388e675fd29d1f1d17f4864318df378b34a3d4d
closerbibi <closerbibi@gmail.com>
2017-02-15 20:26:46 +0800

    crappy parallel func works...even slower

--- SUNRGBD/SUNRGBDtoolbox/SUNRGBDtoolbox/slicedData/prepareData.py
+++ SUNRGBD/SUNRGBDtoolbox/SUNRGBDtoolbox/slicedData/prepareData.py
@@ -16,7 +16,52 @@ import matplotlib.pyplot as plt
 import math
 import pylab
 import matplotlib.patches as patches
+from joblib import Parallel, delayed
+from multiprocessing import Pool
+import itertools
 
+
+def normalize_data():
+    print 'iamempty'
+
+
+def compute_mean():
+    print "Hello, I am empty"
+
+
+def constructing_grid(max_idxy,ix,iy,grid,large_layerpc):
+    rviy = max_idxy - iy
+    # compute density: channel 0
+    location = (large_layerpc[0]==ix)*(large_layerpc[1]==iy)
+    grid[0][rviy][ix] = np.sum((large_layerpc[0]==ix)*(large_layerpc[1]==iy))
+    # find max: channel 1
+    if grid[0][rviy][ix] == 0:   # if empty, giving lowest value of current layer
+        grid[1][rviy][ix] = np.min(large_layerpc[2])
+    else:
+        grid[1][rviy][ix] = np.max(large_layerpc[2][location])
+    return grid
+
+def func_star(a_b):
+    """Convert `f([1,2])` to `f(1,2)` call."""
+    return constructing_grid(*a_b)
+
+'''
+def constructing_grid(max_idxy,ix,iy,grid,large_layerpc):
+    #idxx = inparam[0]; idxy = inparam[1]; grid = inparam[2]; large_layerpc = inparam[3]
+    pdb.set_trace()
+    #for ix in idxx:
+    #    for iy in idxy:
+    rviy = max_idxy - iy
+    # compute density: channel 0
+    location = (large_layerpc[0]==ix)*(large_layerpc[1]==iy)
+    grid[0][rviy][ix] = np.sum((large_layerpc[0]==ix)*(large_layerpc[1]==iy))
+    # find max: channel 1
+    if grid[0][rviy][ix] == 0:   # if empty, giving lowest value of current layer
+        grid[1][rviy][ix] = np.min(large_layerpc[2])
+    else:
+        grid[1][rviy][ix] = np.max(large_layerpc[2][location])
+    return grid
+'''
 def plot_2d_pc_bbox(grid, xmin, xmax, ymin, ymax):
     fig = pylab.figure()
     ax = fig.add_subplot(111, aspect='equal')
@@ -40,11 +85,7 @@ def plot_2d_pc_bbox(grid, xmin, xmax, ymin, ymax):
     ax.set_ylabel('Y Label')
     plt.show()
     plt.show(block=False)
-    pdb.set_trace()
 
-
-def compute_mean():
-    print "Hello, I am empty"
 
 def plot_3d(layer, xmin, xmax, ymin, ymax, zmin, zmax, layers):
     fig = plt.figure()
@@ -103,34 +144,49 @@ def slicedto2D(pc, normals, imagenum, xmin, xmax, ymin, ymax, zmin, zmax, poolin
     idxx = range(int(np.min(largepc[0])),int(np.max(largepc[0])+1),1)
     idxy = range(int(np.min(largepc[1])),int(np.max(largepc[1])+1),1)
     data_grid = np.meshgrid(idxx,idxy)
-    grid = np.zeros((len(layer),2,len(idxy),len(idxx)))
-    for ilayer in xrange(len(layer)): #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    for ilayer in layer.keys(): #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
         layerpcx_shift = layer[ilayer][0]-np.min(pc[0])
         layerpcy_shift = layer[ilayer][1]-np.min(pc[1])
         layerlargex = np.floor(layerpcx_shift*100)
         layerlargey = np.floor(layerpcy_shift*100)
         large_layerpc = np.vstack((layerlargex,layerlargey,layer[ilayer][2]))
-        start = time.time()
+        grid = np.zeros((2,len(idxy),len(idxx)))
+        # using parallel computing to boost up
+        #grid = Parallel(n_jobs=num_cores)(delayed(constructing_grid)(idxx,idxy,grid,large_layerpc))
+        pool = Pool()
+        inparam = {}
+        #inparam['idxx'] = idxx; inparam['idxy'] = idxy; inparam['grid'] = grid; inparam['large_layerpc'] = large_layerpc;
+        #inparam = [idxx,idxy,grid,large_layerpc]
+        #grid = pool.map(constructing_grid, inparam)
+        #constructing_grid(idxx,idxy,grid,large_layerpc)
+
+        # max_idxy,ix,iy,grid,large_layerpc
+        max_idxy = max(idxy)
+        for ix in idxx:
+            pool.map(func_star, itertools.izip(itertools.repeat(max_idxy),itertools.repeat(ix),idxy,itertools.repeat(grid),itertools.repeat(large_layerpc)))
+
+        '''
         for ix in idxx:
             for iy in idxy:
                 rviy = max(idxy) - iy
                 # compute density: channel 0
                 location = (large_layerpc[0]==ix)*(large_layerpc[1]==iy)
-                grid[ilayer][0][rviy][ix] = np.sum((large_layerpc[0]==ix)*(large_layerpc[1]==iy))
+                grid[0][rviy][ix] = np.sum((large_layerpc[0]==ix)*(large_layerpc[1]==iy))
                 # find max: channel 1
-                if grid[ilayer][0][rviy][ix] == 0:   # if empty, giving lowest value of current layer
-                    grid[ilayer][1][rviy][ix] = np.min(large_layerpc[2])
+                if grid[0][rviy][ix] == 0:   # if empty, giving lowest value of current layer
+                    grid[1][rviy][ix] = np.min(large_layerpc[2])
                 else:
-                    grid[ilayer][1][rviy][ix] = np.max(large_layerpc[2][location])
-        print 'time'+ str(time.time()-start)
-    pdb.set_trace()
+                    grid[1][rviy][ix] = np.max(large_layerpc[2][location])
+        '''
+
+        np.save('images/picture_%06d_%03d.npy'%(imagenum,ilayer),grid)
     xmin = xmin - np.min(pc[0]); xmax = xmax - np.min(pc[0]); ymin = ymin - np.min(pc[1]); ymax = ymax - np.min(pc[1]);
     xmin = np.floor(xmin*100); xmax = np.floor(xmax*100); ymin = np.floor(ymin*100); ymax = np.floor(ymax*100); #zmin = np.floor(zmin*100); zmax = np.floor(zmax*100);
     ymin = max(idxy) - ymin; ymax = max(idxy) - ymax
-    plot_2d_pc_bbox(grid, xmin, xmax, ymin, ymax)
-    pdb.set_trace()
+    # plot 2d to show box and data
+    #plot_2d_pc_bbox(grid, xmin, xmax, ymin, ymax)
     
-    return grid
+    return grid, ymin, ymax, xmin, xmax
 
 
 data = h5py.File('/media/closerbibi/internal3/3D/understanding/rankpooling/rgbd_data/nyu_v2_labeled/nyu_depth_v2_labeled.mat')
@@ -141,6 +197,7 @@ K = np.array([[5.7616540758591043e+02,0,3.2442516903961865e+02],[0,5.73756197820
 count=1;
 
 for imagenum in xrange(data['depths'].shape[0]):#size(depths,3):
+    print 'now at image: %d' % (imagenum)
     start_time = time.time()
     # bed=157, chair=5, table=19, sofa=83, toilet=124
     try:
@@ -148,7 +205,7 @@ for imagenum in xrange(data['depths'].shape[0]):#size(depths,3):
     except:
         continue
     #points3d = points3d'
-    pc = box_pc['points3d']; clss=box_pc['clss']
+    pc = box_pc['points3d']; clss=box_pc['clss'][0]
     # change bbox to y,x,z
     ymin = box_pc['ymin'][0]; ymax=box_pc['ymax'][0]; xmin=box_pc['xmin'][0]; xmax=box_pc['xmax'][0]; zmin=box_pc['zmin'][0]; zmax=box_pc['zmax'][0];
     #xmin = box_pc['ymin'][0]; xmax=box_pc['ymax'][0]; ymin=box_pc['xmin'][0]; ymax=box_pc['xmax'][0]; zmin=box_pc['zmin'][0]; zmax=box_pc['zmax'][0];
@@ -159,7 +216,12 @@ for imagenum in xrange(data['depths'].shape[0]):#size(depths,3):
     pooling = 0
     #slicedto2D
     
-    [grid, mean] = slicedto2D(pc, normals, imagenum, xmin, xmax, ymin, ymax, zmin, zmax, pooling);
+    grid, ymin, ymax, xmin, xmax = slicedto2D(pc, normals, imagenum, xmin, xmax, ymin, ymax, zmin, zmax, pooling);
+    for k in xrange(len(clss)):
+        fid = open('./label_box_chair/picture_%07d.txt'%(imagenum),'w')
+        if str(clss[k][0]) == 'chair':
+            fid.write('(%d, %d) - (%d, %d) - (%s)'%(xmin[k], ymin[k], xmax[k], ymax[k], str(clss[k][0])))
+        fid.close()
     '''
     %% computing mean
     mean1 = mean1 + mean_all(1);
@@ -171,4 +233,4 @@ for imagenum in xrange(data['depths'].shape[0]):#size(depths,3):
 »···toc;
     disp(imagenum)
     '''
-    print 'image: %d' % (imagenum), 'time: %.2f s' % (time.time()-start_time)
+    print 'time: %.2f s' % (time.time()-start_time)
